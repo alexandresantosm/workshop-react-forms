bd766c2892e37a2c53470235c0f9b6c55d76cb5f
Alexandre Menezes <amenezes031@gmail.com>
2021-09-02 20:02:02 -0300

    add form in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Header } from "./components/Header";
+import { QuotationForm } from "./components/QuotationForm";
 
 const App = (): JSX.Element => {
   return (
     <>
       <Header />
+      <div className="container pt-5 pb-5">
+        <QuotationForm />
+      </div>
     </>
   );
 };
