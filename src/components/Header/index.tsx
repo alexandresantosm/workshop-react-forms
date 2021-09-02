bca5d11a89b79fbc19ab3f260df895ee932c3319
Alexandre Menezes <amenezes031@gmail.com>
2021-09-02 12:19:52 -0300

    feat: add type in component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./../Logo";
 
 import { Container } from "./style";
 
-export const Header = () => {
+export const Header = (): JSX.Element => {
   return (
     <Container>
       <Logo src={logoSVG} height="22px" alt="Logo Localiza" />
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -4,6 +4,6 @@ interface LogoProps {
   alt: string;
 }
 
-export const Logo = ({ src, height, alt }: LogoProps) => {
+export const Logo = ({ src, height, alt }: LogoProps): JSX.Element => {
   return <img src={src} height={height} alt={alt} />;
 };
