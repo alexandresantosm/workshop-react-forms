69f773144b8ae31e4e293ea754154462f4b08858
Alexandre Menezes <amenezes031@gmail.com>
2021-09-02 20:01:40 -0300

    feat: create QuotationForm component

---
+++ src/components/QuotationForm/index.tsx
@@ -0,0 +1,127 @@
+import { FormEvent } from "react";
+import { inputNames } from "../../common/constants/inputNames";
+import { hours } from "../../common/utils/hours";
+import { FormValues } from "../FormValues";
+
+type FormValuesType = {
+  pickUpAgency: string;
+  pickUpDate: string;
+  pickUpHour: string;
+  specialRequest: string;
+};
+
+export const QuotationForm = (): JSX.Element => {
+  const initialValues = {
+    pickUpAgency: "",
+    pickUpDate: "",
+    pickUpHour: "",
+    specialRequest: "",
+  };
+  let formValues: FormValuesType = initialValues;
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formInputs = event.currentTarget.elements;
+
+    formValues = inputNames.reduce(
+      (acc, name) => ({
+        ...acc,
+        // @ts-ignore
+        [name]: formInputs[name].value,
+      }),
+      initialValues
+    );
+
+    console.log(formValues);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="row mb-3">
+          <div className="col-md-5">
+            <label className="form-label" htmlFor="pickUpAgency">
+              Local de retirada
+            </label>
+            <input
+              className="form-control"
+              id="pickUpAgency"
+              name="pickUpAgency"
+              aria-describedby="pickUpAgencyHelp"
+            />
+            <div className="form-text" id="pickUpAgencyHelp">
+              Selecione o local onde deseja retirar o carro.
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <label className="form-label" htmlFor="pickUpDate">
+              Data de retirada
+            </label>
+            <input
+              className="form-control"
+              id="pickUpDate"
+              name="pickUpDate"
+              aria-describedby="pickUpDateHelp"
+            />
+            <div className="form-text" id="pickUpDateHelp">
+              Selecione a data de retirada.
+            </div>
+          </div>
+
+          <div className="col-md-3">
+            <label className="form-label" htmlFor="pickUpHour">
+              Horário de retirada
+            </label>
+            <select
+              className="form-select"
+              id="pickUpHour"
+              name="pickUpHour"
+              aria-describedby="pickUpHourHelp"
+            >
+              {hours.map((value) => (
+                <option key={`option-${value}`} value={value}>
+                  {value}
+                </option>
+              ))}
+            </select>
+            <div className="form-text" id="pickUpHourHelp">
+              Selecione a hora de retirada.
+            </div>
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <div className="col-md-12">
+            <label className="form-label" htmlFor="specialRequest">
+              Pedido especial
+            </label>
+
+            <textarea
+              className="form-control"
+              id="specialRequest"
+              name="specialRequest"
+              aria-describedby="specialRequestHelp"
+            />
+            <div className="form-text" id="specialRequestHelp">
+              Esse é um espaço destinado especialmente para você nos contar como
+              podemos lhe atender melhor.
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <button className="btn btn-primary" type="submit">
+              Enviar
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className="mt-3">
+        <FormValues values={formValues} />
+      </div>
+    </>
+  );
+};
